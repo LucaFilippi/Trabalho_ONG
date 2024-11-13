b73e48893ac0d9bcc907b36cfaadb06f81ad4969
Cecilia Gontijo <ceciliagontijo50@gmail.com>
2024-11-13 11:42:26 -0300

    comit

---
+++ teste2/index.js
@@ -0,0 +1,22 @@
+
+
+function login() {
+    firebase.auth().signInWithEmailAndPassword(
+        form.text().value, form.email().value, form.tel().value
+    ).then(response => {
+        window.location.href = "pages/home/home.html";
+    }).catch(error => {
+        alert(getErrorMessage(error));
+    });
+}
+
+function getErrorMessage(error) {
+    if (error.code == "auth/user-not-found") {
+        return "Usuário nao encontrado";
+    }
+    return error.message;
+}
+
+function cadastro() {
+    window.location.href = "index.html";
+}
